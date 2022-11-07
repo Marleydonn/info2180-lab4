3a983954e171ed462219d06cc9a2c239963cd919
Jahmarley Robinson <jahmarley2002@gmail.com>
2022-11-07 17:33:47 -0500

    Edited files to complete exercises

---
+++ app.js
@@ -0,0 +1,29 @@
+function getAllSuperHeroes() {
+    return fetch("http://localhost/info2180-lab4/superheroes.php")
+      .then((result) => result.text())
+      .then((data) => data)
+      .catch((err) => err);
+  }
+  function sanitizeInput(str) {
+    str = str.replace(/[^a-z0-9áéíóúñü \.,_-]/gim, " ");
+    return str.trim();
+  }
+  function getSpecificHero(hero) {
+    return fetch(`http://localhost/info2180-lab4/superheroes.php?query=${hero}`)
+      .then((result) => result.text())
+      .then((data) => data)
+      .catch((err) => err);
+  }
+  
+  function searchHeroes(search) {
+    let newSearch = sanitizeInput(search);
+    if (search === null || newSearch === "") return getAllSuperHeroes();
+    return getSpecificHero(newSearch);
+  }
+  
+  document.getElementById("search-all").addEventListener("click", () => {
+    searchHeroes(document.getElementById("superhero-search").value).then(
+      (data) => (document.getElementById("result").innerHTML = data)
+    );
+  });
+  
